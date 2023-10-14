a06c4573525766a4976beaba1dda806102c2cd22
Sara Bonnin <saesbori@gmail.com>
2023-10-14 21:09:10 +0100

    remove BookDaysOff and center all in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import { Col, Row } from "react-bootstrap";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import BookDaysOff from "./components/BookDaysOff";
+import CalendarPicker from "./components/calendar/CalendarPicker";
+import BookingButton from "./components/calendar/BookingButton";
+import UpcomingAccordion from "./components/accordionUpcomingView/UpcomingAccordion";
+import ListPickedDays from "./components/calendar/ListPickedDays";
+import TrackingTable from "./components/trackingTable/TrackingTable";
 import GetBankHolidays from "./components/data/GetBankHolidays";
 
 function App() {
@@ -14,19 +19,57 @@ function App() {
 
   const bankHolidays = GetBankHolidays();
 
+  const [daysBooked, setDaysBooked] = useState([]);
+  const [daysPicked, setDaysPicked] = useState([]);
+
   return (
     <>
       <div className="content-wrap">
         <Header />
         <div className="content">
-          <BookDaysOff
-            allowance={allowance}
-            allowanceUsed={allowanceUsed}
-            setAllowanceUsed={setAllowanceUsed}
-            allowanceAvailable={allowanceAvailable}
-            setAllowanceAvailable={setAllowanceAvailable}
-            bankHolidays={bankHolidays}
-          />
+          <Row>
+            <Col>
+              <CalendarPicker
+                bankHolidays={bankHolidays}
+                allowance={allowance}
+                allowanceUsed={allowanceUsed}
+                allowanceAvailable={allowanceAvailable}
+                setAllowanceUsed={setAllowanceUsed}
+                setAllowanceAvailable={setAllowanceAvailable}
+                daysBooked={daysBooked}
+                setDaysBooked={setDaysBooked}
+                daysPicked={daysPicked}
+                setDaysPicked={setDaysPicked}
+              />
+              <BookingButton
+                daysPicked={daysPicked}
+                setDaysPicked={setDaysPicked}
+                allowanceAvailable={allowanceAvailable}
+                setDaysBooked={setDaysBooked}
+                allowanceUsed={allowanceUsed}
+                setAllowanceUsed={setAllowanceUsed}
+                setAllowanceAvailable={setAllowanceAvailable}
+                daysBooked={daysBooked}
+              />
+              <ListPickedDays daysPicked={daysPicked} />
+            </Col>
+            <Col>
+              <Row>
+                <TrackingTable
+                  className="main-tracking-table"
+                  allowance={allowance}
+                  allowanceUsed={allowanceUsed}
+                  allowanceAvailable={allowanceAvailable}
+                />
+              </Row>
+              <Row>
+                <UpcomingAccordion
+                  bankHolidays={bankHolidays}
+                  daysBooked={daysBooked}
+                />
+              </Row>
+            </Col>
+          </Row>
         </div>
         <Footer />
       </div>
